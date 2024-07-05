331f0e23399ad02a648f0d23b4d08a80ac22d6f9
Manuel Cruz <amcp.negocios@gmail.com>
2024-07-04 18:31:48 -0600

    Updated auth and jwt

--- src/lib/jwt.js
+++ src/lib/jwt.js
@@ -1,21 +1,27 @@
-const jsonwebtoken = require('jsonwebtoken')
-const createError = require('http-errors')
+const jsonwebtoken = require('jsonwebtoken');
+const createError = require('http-errors');
 
-const { JWT_SECRET } = process.env
+const { JWT_SECRET } = process.env;
 
-function sign(payload){
-    return jsonwebtoken.sign(payload, JWT_SECRET, { expiresIn: '1d' })
+function sign(payload) {
+    return jsonwebtoken.sign(payload, JWT_SECRET, { expiresIn: '1d' });
 }
 
 function verify(token) {
     try {
+        if (!token) {
+            throw createError(401, 'Token not provided');
+        }
         return jsonwebtoken.verify(token, JWT_SECRET);
     } catch (error) {
-        throw createError(401, 'Invalid or expired token');
+        if (error.name === 'TokenExpiredError') {
+            throw createError(401, 'Token expired');
+        }
+        throw createError(401, 'Invalid token');
     }
 }
 
 module.exports = {
     sign,
     verify
-}
+};
--- src/middlewares/auth.middleware.js
+++ src/middlewares/auth.middleware.js
@@ -1,30 +1,27 @@
-const createError = require('http-errors')
-const jwt = require('../lib/jwt')
-const useCase = require('../usecases/users.usecase')
+const createError = require('http-errors');
+const jwt = require('../lib/jwt');
+const useCase = require('../usecases/users.usecase');
 
-async function auth(req, res, next){
-    try{
-        const token = req.headers.authorization
+async function auth(req, res, next) {
+    try {
+        const token = req.headers.authorization || localStorage.getItem('token'); // Check localStorage for token
         
-        if(!token){
-            throw createError(401, "JWT is required")
+        if (!token) {
+            throw createError(401, "JWT is required");
         }
 
-        const payload = jwt.verify(token)
-        
-        const user = await useCase.getById(payload.id)
+        const payload = jwt.verify(token);
+        const user = await useCase.getById(payload.id);
+        req.user = user;
 
-        req.user = user
+        next();
 
-        next()
-
-    } catch(error){
-        res.status(401)
-        res.json({
-            sucess:false,
+    } catch (error) {
+        res.status(401).json({
+            success: false,
             error: error.message
-        })
+        });
     }
 }
 
-module.exports = auth 
+module.exports = auth;
